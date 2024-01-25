035aa97f7d4f1d72e912c2c43de288dfc1147e31
danoshi <39885406+danoshi@users.noreply.github.com>
2024-01-25 22:58:38 +0100

    done coffee machine project

--- Coffee Machine (Python)/task/machine/coffee_machine.py
+++ Coffee Machine (Python)/task/machine/coffee_machine.py
@@ -1,38 +1,84 @@
 # Write your code here
-
-def water(cups: int) -> int:
-    return int(cups / 200)
-
-
-def milk(cups: int) -> int:
-    return int(cups / 50)
+water = 400
+milk = 540
+coffee = 120
+cups = 9
+money = 550
 
 
-def beans(cups: int) -> int:
-    return int(cups / 15)
+def echo() -> None:
+    print("The coffee machine has:")
+    print(f"{water} ml of water")
+    print(f"{milk} ml of milk")
+    print(f"{coffee} g of coffee beans")
+    print(f"{cups} disposable cups")
+    print(f"${money} of money")
 
 
-def coffee() -> None:
-    cup = []
-    print("Write how many cups of coffee you will need:")
+def buy() -> None:
+    global water, milk, coffee, cups, money
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    i = input()
+    if i == "back":
+        return
+    elif int(i) == 1 and water >= 250 and coffee >= 16 and cups >= 1:
+        water -= 250
+        coffee -= 16
+        cups -= 1
+        money += 4
+    elif int(i) == 2 and water >= 350 and coffee >= 20 and cups >= 1 and milk >= 75:
+        water -= 350
+        milk -= 75
+        coffee -= 20
+        cups -= 1
+        money += 7
+    elif int(i) == 3 and water >= 200 and coffee >= 12 and cups >= 1 and milk >= 100:
+        water -= 200
+        milk -= 100
+        coffee -= 12
+        cups -= 1
+        money += 6
+    else:
+        print(f"Sorry, not enough resources!")
+
+
+def fill() -> None:
+    global water, milk, coffee, cups
+    print("Write how many ml of water you want to add: ")
     w = int(input())
-    cup.append(water(w))
-    print("Write how many ml of milk the coffee machine has:")
+    print("Write how many ml of milk you want to add: ")
     m = int(input())
-    cup.append(milk(m))
-    print("Write how many grams of coffee beans the coffee machine has:")
-    b = int(input())
-    cup.append(beans(b))
-    print("Write how many cups of coffee you will need:")
-    a = int(input())
-    cups = min(cup)
-    if a == cups:
-        print("Yes, I can make that amount of coffee")
-    elif a < cups:
-        cups = cups - a
-        print(f"Yes, I can make that amount of coffee (and even {cups} more than that)")
-    elif a > cups:
-        print(f"No, I can make only {cups} cups of coffee")
+    print("Write how many grams of coffee beans you want to add: ")
+    cof = int(input())
+    print("Write how many disposable cups you want to add: ")
+    cup = int(input())
+    water += w
+    milk += m
+    coffee += cof
+    cups += cup
 
 
-coffee()
+def take() -> None:
+    global money
+    print(f"I gave you ${money}")
+    money -= money
+
+
+def main() -> None:
+    a = True
+    while a:
+        print("Write action (buy, fill, take, remaining, exit): ")
+        i = input()
+        if i == "buy":
+            buy()
+        elif i == "fill":
+            fill()
+        elif i == "take":
+            take()
+        elif i == "remaining":
+            echo()
+        elif i == "exit":
+            a = False
+
+
+main()
